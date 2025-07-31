5aec491a494cdedb0e6be5aadb7d013fa4738e1a
전지원 <wjseldnjs2@douzone.com>
2025-07-31 17:49:51 +0900

    스프링 빈 config 개선, test 코드작성, xml 추가

--- core/src/main/java/spring/core/AppConfig.java
+++ core/src/main/java/spring/core/AppConfig.java
@@ -1,6 +1,11 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
@@ -10,13 +15,30 @@ import spring.core.order.OrderServiceImpl;
 /**
  * 애플리케이션 실제 동작에 필요한 구현 객체를 생성한다.
  * 생성한 객체 인스턴스를 생성자를 통해서 주입(연결)해준다.
+ *
+ * 리펙토링 : 중복을 제거하고 역할에 따른 구현이 보이도록 수정 (memberRepository, discountPolicy 함수 추가)
+ * 리팩토링 : 순수 자바 코드 -> 스프링으로 전환 (ApplicationContext -> @Configuration, @Bean 어노테이션 추가)
  * */
+@Configuration
 public class AppConfig {
+  @Bean
   public MemberService memberService(){
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
-
+  @Bean
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(
+            memberRepository(),
+            discountPolicy()
+    );
+  }
+  @Bean
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+  @Bean
+  public DiscountPolicy discountPolicy() {
+//    return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
   }
 }
--- core/src/main/java/spring/core/OrderApp.java
+++ core/src/main/java/spring/core/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
@@ -11,9 +13,14 @@ import spring.core.order.OrderServiceImpl;
 public class OrderApp {
 
   public static void main(String[] args) {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+//    OrderService orderService = appConfig.orderService();
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+    OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
     long memberId = 1L;
     Member member = new Member(memberId, "memberA", Grade.VIP);
     memberService.join(member);
